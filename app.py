9008f1867f0f71c13591f2ea0014e5d2229890ac
Lucas Bessa <lucasbessa708@gmail.com>
2020-12-13 19:59:00 -0300

    interface pronta para apresentaçao

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from chatbot import chatbot
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 app.static_folder = 'static'
